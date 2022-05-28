696cf5fc213f05be70a20f742f0022ddf144124e
Александр Абрамов <alexabramov2201@yandex.ru>
2022-05-28 13:50:23 +0300

    1 ghdjthrf

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
     list_editable = ('group',)
