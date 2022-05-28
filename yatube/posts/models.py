86b07892478d1d21ece70d3078f16c283240927b
Александр Абрамов <alexabramov2201@yandex.ru>
2022-05-28 13:39:41 +0300

    1 ghdjthrf

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,17 +1,18 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
+
 class Group(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    
-    def __str__(self):
-        return str(self.title)
 
-       
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -21,9 +22,8 @@ class Post(models.Model):
         related_name='posts'
     )
     group = models.ForeignKey(
-        Group, 
-        blank=True, 
-        null=True, 
+        Group,
+        blank=True,
+        null=True,
         on_delete=models.CASCADE
     )
-
