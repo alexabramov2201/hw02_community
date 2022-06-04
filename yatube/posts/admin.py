01953463528373c60b84d5aa5c9666fd30328e8e
Your Name <you@example.com>
2022-06-04 04:41:22 +0300

    готово

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -11,6 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'description')
