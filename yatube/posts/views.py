83668edf83d36e91bd68744a93296f3bcbfbcdaf
Александр Абрамов <aleksandrabramov@MacBook-Pro-Aleksandr.local>
2022-06-07 23:00:39 +0300

    проверка

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts[:CURRENT_POSTS]
+    posts = group.posts[:CURRENT_POSTS].
     context = {
         'group': group,
         'posts': posts,
